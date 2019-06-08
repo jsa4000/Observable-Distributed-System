03bd1c2dfc2d90d7b209fc16eb2885bd1699f928
Javier Santos Andrés <jsantosa@MacBook-Pro.local>
2019-06-08 11:16:33 +0200

    Added metrics starter and refactorized dependencies

--- src/spring-core-project/spring-core-starter-metrics/src/main/java/com/spring/core/metrics/autoconfigure/MeterRegistryAutoConfiguration.java
+++ src/spring-core-project/spring-core-starter-metrics/src/main/java/com/spring/core/metrics/autoconfigure/MeterRegistryAutoConfiguration.java
@@ -1,21 +1,24 @@
-package com.group.ecommerce.catalog.config;
+package com.spring.core.metrics.autoconfigure;
 
 import io.micrometer.core.aop.TimedAspect;
 import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class MeterRegistryConfig {
+public class MeterRegistryAutoConfiguration {
 
     @Bean
+    @ConditionalOnMissingBean
     public MeterRegistryCustomizer<MeterRegistry> metricsCommonTags(@Value("${spring.application.name:}") String appName) {
         return registry -> registry.config().commonTags("application", appName);
     }
 
     @Bean
+    @ConditionalOnMissingBean
     public TimedAspect timedAspect(MeterRegistry registry) {
         return new TimedAspect(registry);
     }
---
+++ src/spring-core-project/spring-core-starter-metrics/src/test/java/com/spring/core/metrics/autoconfigure/MeterRegistryAutoConfigurationTest.java
@@ -0,0 +1,11 @@
+package com.spring.core.metrics.autoconfigure;
+
+import org.junit.Test;
+
+public class MeterRegistryAutoConfigurationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
