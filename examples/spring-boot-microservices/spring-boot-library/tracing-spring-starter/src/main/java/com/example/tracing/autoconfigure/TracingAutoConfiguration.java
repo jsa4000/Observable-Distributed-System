e1c779afb4da81d08b48052a5e9d3d03d7689e12
Javier Santos Andrés <jsa4000@gmail.com>
2021-06-14 12:47:34 +0200

    Added tracing module using using Jaeger

--- examples/spring-boot-microservices/spring-boot-library/tracing-spring-starter/src/main/java/com/example/tracing/autoconfigure/TracingAutoConfiguration.java
+++ examples/spring-boot-microservices/spring-boot-library/tracing-spring-starter/src/main/java/com/example/tracing/autoconfigure/TracingAutoConfiguration.java
@@ -1,8 +1,11 @@
 package com.example.tracing.autoconfigure;
 
+import com.example.core.factory.YamlPropertySourceFactory;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource(value = "classpath:tracing.yaml", factory = YamlPropertySourceFactory.class, encoding = "UTF-8")
 public class TracingAutoConfiguration {
 
 }
